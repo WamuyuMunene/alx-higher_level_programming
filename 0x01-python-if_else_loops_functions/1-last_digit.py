751e0ed98ad58b64f9a95dfc181d9b4077a93667
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-13 01:25:29 -0700

    indexing in int and print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+number = str(number)
 if number[-1] > 5:
     print(f"Last digit of {number} is {number[-1]} and is greater than 5")
 elif number[-1] == 0:
