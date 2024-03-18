e98eeaa3a3f8b1ebaba135e83f971734d82622f2
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-18 13:00:45 -0700

    sums by index

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum1 = 0
-    sum2 = 0
-    if len(tuple_a - 1) < 2:
-        tuple_a += (0, 0)
-    elif len(tuple_b - 1) < 2:
-        tuple_b += (0, 0)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
     sum1 = tuple_a[0] + tuple_b[0]
     sum2 = tuple_a[1] + tuple_b[1]
     return sum1, sum2
