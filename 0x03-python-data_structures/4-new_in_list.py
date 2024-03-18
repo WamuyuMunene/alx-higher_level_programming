35d03a0a9f13d475d09f781d48417c1315b7519d
WamuyuMunene <wamuyumunene98@gmail.com>
2024-03-18 12:07:38 -0700

    modify element and return copy of list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
     else:
-        my_list_new = my_list
+        my_list_new = my_list.copy()
         my_list_new[idx] = element
         return my_list_new
